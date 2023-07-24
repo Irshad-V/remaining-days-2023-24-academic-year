914896a2c2898cb8c48e8e3a9178884a92a810ae
Irshad-V <113372096+Irshad-V@users.noreply.github.com>
2023-07-24 15:00:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -339,7 +339,7 @@ function updateQuoteEvery24Hours() {
 
     setInterval(() => {
         displayQuote(); // Display a new random quote every 24 hours
-    }, 5 * 1000); //
+    }, 7 * 1000); //
 }
 //   24 * 60 * 60
 updateQuoteEvery24Hours();
